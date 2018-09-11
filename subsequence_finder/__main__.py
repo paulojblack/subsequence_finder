2cf8fe8172402d573061149a28aadc5a61f1a789
Paulo Black <pblack@goodwaygroup.com>
2018-09-11 12:32:28 -0400

    enlargen default input array

--- subsequence_finder/__main__.py
+++ subsequence_finder/__main__.py
@@ -4,7 +4,7 @@ from .subseq_finder import find_consecutive_runs
 
 
 def main():
-    input_array = [1, 2, 3, 4, 3, 2]
+    input_array = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7]
     result = find_consecutive_runs(input_array)
 
     print(result)
